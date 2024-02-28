837fd64ebc53cfcf23d1172530ecb7e61cb5f997
Vadim <vadimka.abazov@gmail.com>
2024-02-29 03:27:19 +0700

    hw1

--- tests/test_item.py
+++ tests/test_item.py
@@ -12,3 +12,28 @@ def test_apply_discount():
     data.pay_rate = 0.8
     data.apply_discount()
     assert data.price == 8000.0
+
+
+def test_name_setter():
+    item = Item('Телефон', 10000, 5)
+    item.name = 'Смартфон'
+    assert item.name == 'Смартфон'
+
+
+def test_name_setter_truncate():
+    item = Item('Телефон', 25000, 3)
+    item.name = 'Суперсмартфон'
+    assert item.name == 'Суперсмарт'
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv('../src/items.csv')
+    assert len(Item.all) == 5
+    item1 = Item.all[0]
+    assert item1.name == 'Смартфон'
+
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
